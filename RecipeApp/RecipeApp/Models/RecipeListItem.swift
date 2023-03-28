66de64105813540a6394c469ffedf462601ed47f
Nathan Wale <nathan.wale@icloud.com>
2023-03-28 12:18:54 +1100

    Initial commit: Model for recipe list items

---
+++ RecipeApp/RecipeApp/Models/RecipeListItem.swift
@@ -0,0 +1,59 @@
+//
+//  RecipeEntry.swift
+//  RecipeApp
+//
+//  Created by Nathan Wale on 28/3/2023.
+//
+
+import Foundation
+
+// recipe ids are strings
+typealias RecipeIdentifier = String
+
+
+//
+// Item for recipe lists
+//  - name: name of recipe
+//  - imageUrl: URL of image
+//  - identifier: id of recipe in database
+//
+struct RecipeListItem
+{
+    let name: String
+    let imageUrl: URL
+    let identifier: RecipeIdentifier
+}
+
+
+//
+// Codable implementation
+//
+extension RecipeListItem: Codable
+{
+    // JSON keys for values
+    enum CodingKeys: String, CodingKey
+    {
+        case name = "strMeal"
+        case imageUrl = "strMealThumb"
+        case identifier = "idMeal"
+    }
+}
+
+
+//
+// Hashable implementation
+//
+extension RecipeListItem: Hashable
+{
+    // hash function, use recipe name
+    func hash(into hasher: inout Hasher)
+    {
+        hasher.combine(name)
+    }
+    
+    // equals, use identifier
+    static func ==(a: RecipeListItem, b: RecipeListItem) -> Bool
+    {
+        a.identifier == b.identifier
+    }
+}
