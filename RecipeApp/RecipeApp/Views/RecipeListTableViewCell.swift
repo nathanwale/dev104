8c3c211104c97792162fd091d45ccf1925e456cb
Nathan Wale <nathan.wale@icloud.com>
2023-04-20 12:05:16 +1000

    Table Cell View to display RecipeListItems

---
+++ RecipeApp/RecipeApp/Views/RecipeListTableViewCell.swift
@@ -0,0 +1,61 @@
+//
+//  RecipeListTableViewCell.swift
+//  RecipeApp
+//
+//  Created by Nathan Wale on 19/4/2023.
+//
+
+import UIKit
+
+class RecipeListTableViewCell: UITableViewCell
+{
+    // MARK: - outlets
+    @IBOutlet var recipeImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    
+    
+    // MARK: - properties
+    var fetchImageTask: Task<Void, Never>? = nil
+    
+    
+    // MARK: - lifecycle
+    // cancel fetch image task on deinit
+    deinit {
+        fetchImageTask?.cancel()
+    }
+    
+    
+    // MARK: - update
+    func update(with recipeListItem: RecipeListItem)
+    {
+        nameLabel.text = recipeListItem.name
+        fetchImage(for: recipeListItem.imageUrl)
+        recipeImageView.layer.cornerRadius = 10
+    }
+    
+    // MARK: - remote requests
+    //
+    // fetch and update recipe image
+    //
+    func fetchImage(for url: URL)
+    {
+        let request = ImageRequest(url: url)
+        
+        // cancel image task if it exists
+        fetchImageTask?.cancel()
+        
+        // start task to fetch image
+        Task {
+            do {
+                // fetch image and assign to `recipeImageView`
+                let image = try await request.send()
+                recipeImageView.image = image
+            } catch {
+                // error
+                print(error)
+            }
+            // finish task
+            fetchImageTask = nil
+        }
+    }
+}
