decf56bf551f916c2a9ded044c295d1ddbdc6d5a
Nathan Wale <nathan.wale@icloud.com>
2023-05-04 17:58:57 +1000

    Highlight newly saved recipes. + newlyAddedRecipes: list of just saved recipes + markNewRecipes: marks recipes as new and updates datasource. Called in viewWillAppear + unmarkNewRecipes: empties new recipes list and returns default style to new recipes. Called in viewDidDisappear

--- RecipeApp/RecipeApp/View Controllers/FavouriteRecipesViewController.swift
+++ RecipeApp/RecipeApp/View Controllers/FavouriteRecipesViewController.swift
@@ -9,6 +9,60 @@ import UIKit
 
 class FavouriteRecipesViewController: RecipeListTableViewController
 {
+    // items that have recently been added
+    var newlyAddedRecipes = [RecipeListItem]()
+    
+    
+    // MARK: - lifecycle
+    //
+    // view will appear:
+    //   - mark newly added recipes
+    //
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        markNewRecipes()
+    }
+    
+    //
+    // view did disappear:
+    //   - unmark newly added recipes
+    //
+    override func viewDidDisappear(_ animated: Bool)
+    {
+        super.viewDidDisappear(animated)
+        unmarkNewRecipes()
+    }
+    
+    // MARK: - style
+    //
+    // mark new items
+    //
+    func markNewRecipes()
+    {
+        for i in newlyAddedRecipes.indices {
+            newlyAddedRecipes[i].newlyAdded = true
+        }
+
+        updateItems(newlyAddedRecipes)
+    }
+    
+    
+    //
+    // unmark new items, empty newly added recipes
+    //
+    func unmarkNewRecipes()
+    {
+        for i in newlyAddedRecipes.indices {
+            newlyAddedRecipes[i].newlyAdded = false
+        }
+
+        updateItems(newlyAddedRecipes)
+        newlyAddedRecipes = []
+    }
+    
+    
+    // MARK: - datasource
     //
     // Load saved recipes from UserStore
     //
@@ -23,6 +77,7 @@ class FavouriteRecipesViewController: RecipeListTableViewController
     func recipeWasSaved(_ recipe: RecipeListItem)
     {
         addItem(recipe)
+        newlyAddedRecipes.append(recipe)
     }
     
     //
@@ -31,5 +86,6 @@ class FavouriteRecipesViewController: RecipeListTableViewController
     func recipeWasUnsaved(_ recipe: RecipeListItem)
     {
         removeItem(recipe)
+        newlyAddedRecipes.removeAll { $0 == recipe }
     }
 }
