4985dd8e2cb40f38ec1ebfe4dc486d2b7f48a9ed
Nathan Wale <nathan.wale@icloud.com>
2023-05-04 11:32:04 +1000

    - recipeWasSaved/Unsaved: add or remove item when saved/unsaved

--- RecipeApp/RecipeApp/View Controllers/FavouriteRecipesViewController.swift
+++ RecipeApp/RecipeApp/View Controllers/FavouriteRecipesViewController.swift
@@ -9,8 +9,27 @@ import UIKit
 
 class FavouriteRecipesViewController: RecipeListTableViewController
 {
+    //
+    // Load saved recipes from UserStore
+    //
     override func loadItems()
     {
         replaceItems(items: UserStore.savedRecipes)
+    }
+    
+    //
+    // recipe was saved, add to snapshot
+    //
+    func recipeWasSaved(_ recipe: RecipeListItem)
+    {
+        addItem(recipe)
+    }
+    
+    //
+    // recipe was unsaved, remove from snapshot
+    //
+    func recipeWasUnsaved(_ recipe: RecipeListItem)
+    {
+        removeItem(recipe)
     }
 }
