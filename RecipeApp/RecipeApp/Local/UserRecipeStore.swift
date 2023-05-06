fb2bd64c91c768a3e6d6e0a41bed75c58068398e
Nathan Wale <nathan.wale@icloud.com>
2023-05-06 11:44:54 +1000

    - UserRecipeStore conforms to LocalStore to provide saving and loading from a local file. - Saved recipes are now accessed through `UserRecipeStore.shared`. - Automatically loads from file on init - Automatically saves to file on save() and unsave() - Removed placeholder saved recipes

--- RecipeApp/RecipeApp/Local/UserRecipeStore.swift
+++ RecipeApp/RecipeApp/Local/UserRecipeStore.swift
@@ -7,25 +7,80 @@
 
 import Foundation
 
-fileprivate let demoUrl = URL(string: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2Ff0%2Ff0%2Fec%2Ff0f0ec88688c01b59661240899e57d83--people-eating-intelligent-people.jpg&f=1&nofb=1&ipt=01caad78c899c7f2b5367353fff0021ed344d33e295f59fa32f980617bd5d55d&ipo=images")!
-
+//
+// Storage for saved recipes
+//
 struct UserRecipeStore
 {
-    var savedRecipes = [
-        RecipeListItem(name: "Nutrient Paste", imageUrl: demoUrl, identifier: "1"),
-        RecipeListItem(name: "Oyako Don", imageUrl: demoUrl, identifier: "2"),
-        RecipeListItem(name: "Lanzhou Lamian", imageUrl: demoUrl, identifier: "3"),
-        RecipeListItem(name: "Tortellini", imageUrl: demoUrl, identifier: "4"),
-    ]
+    // shared instance
+    static var shared = UserRecipeStore()
     
-    mutating func save(recipe: RecipeListItem)
+    // saved recipes
+    var savedRecipes = [RecipeListItem]()
+    
+    
+    //
+    // Private init: loads savedRecipes
+    //   * catches and prints errors
+    //
+    private init()
     {
-        savedRecipes.append(recipe)
+        do {
+            savedRecipes = try loadFromFile()
+        } catch {
+            print(error)
+        }
     }
     
-    mutating func unsave(recipe: RecipeListItem) {
-        savedRecipes.removeAll {
-            $0 == recipe
+    
+    //
+    // Save recipe
+    //   * catches and prints errors
+    //
+    mutating func save(recipe: RecipeListItem)
+    {
+        do {
+            savedRecipes.append(recipe)
+            try saveToFile(store: savedRecipes)
+        } catch {
+            print(error)
         }
+    }
+    
+    
+    //
+    // Unsave recipe
+    //   * catches and prints errors
+    //
+    mutating func unsave(recipe: RecipeListItem)
+    {
+        do {
+            savedRecipes.removeAll {
+                $0 == recipe
+            }
+            try saveToFile(store: savedRecipes)
+        } catch {
+            print(error)
+        }
+    }
+}
+
+
+//
+// LocalStore conformance
+//   - allows loading from and saving to local files
+//
+extension UserRecipeStore: LocalStore
+{
+    typealias StoreType = [RecipeListItem]
+    
+    // name of file to save to
+    var fileName: String {
+        "saved-recipes"
+    }
+    
+    // new representation of store type
+    func newStore() -> [RecipeListItem] {
+        []
     }
 }
