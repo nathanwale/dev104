8708550ffeec0407b714669eb11e703d856695ad
Nathan Wale <nathan.wale@icloud.com>
2023-05-15 15:25:45 +1000

    Remote API Request: Recipes for a given category

---
+++ RecipeApp/RecipeApp/Remote/RecipesForCategoryRequest.swift
@@ -0,0 +1,44 @@
+//
+//  CategoryListRequest.swift
+//  RecipeApp
+//
+//  Created by Nathan Wale on 11/4/2023.
+//
+
+import Foundation
+
+struct RecipesForCategoryRequest: ApiRequest
+{
+    typealias Response = [String: [RecipeListItem]?]
+    
+    var category: RecipeCategory
+        
+    var subPath: String {
+        "filter.php"
+    }
+    
+    var queryItems: [URLQueryItem]? {
+        [URLQueryItem(name: "c", value: category)]
+    }
+    
+    func fetchRecipes() async throws -> [RecipeListItem]
+    {
+        // send() returns a dictionary with one key ["meals"]
+        let result = try await send()
+        
+        // if the key doesn't exist, throw error
+        // this is an unusual case
+        guard let recipes = result["meals"] else {
+            throw ApiRequestError.requestFailed
+        }
+        
+        // if the key exists, are there any results?
+        if let recipes = recipes {
+            // found some results, return them
+            return recipes
+        } else {
+            // no results, return empty array
+            return []
+        }
+    }
+}
