531d2f56211d3a7161ba5943d2ca011e292ae255
Nathan Wale <nathan.wale@icloud.com>
2023-05-17 11:26:07 +1000

    + SaveRecipeDelegate.recipeListControllers / .registerRecipeListControllers: So that Save delegate knows which View Controllers to notify when a recipe is saved
    + RequiresSaveRecipeDelegate: protocol to simplify finding controllers that need a save delegate

--- RecipeApp/RecipeApp/Delegates/SaveRecipeDelegate.swift
+++ RecipeApp/RecipeApp/Delegates/SaveRecipeDelegate.swift
@@ -7,8 +7,26 @@
 
 import Foundation
 
+//
+// Delegate for handling saving of recipes
+//   - Notifies recipe list controllers that a recipe has been saved
+//
 protocol SaveRecipeDelegate
 {
+    // save and unsave
     func save(recipe: RecipeListItem)
     func unsave(recipe: RecipeListItem)
+    
+    // recipe list controllers
+    var recipeListControllers: [RecipeListTableViewController] { get set }
+    func registerRecipeListController(_ controller: RecipeListTableViewController)
+}
+
+
+//
+// Protocol that guarantees user has a SaveRecipeDelegate
+//
+protocol RequiresSaveRecipeDelegate
+{
+    var saveRecipeDelegate: SaveRecipeDelegate! { get set }
 }
