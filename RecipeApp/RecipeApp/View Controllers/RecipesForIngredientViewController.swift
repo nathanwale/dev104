acf88b56dda7ca4b239923fe7fad04f9299620b0
Nathan Wale <nathan.wale@icloud.com>
2023-05-18 14:03:39 +1000

    View Controller to display all Recipes for a given Ingredient.
    Inherits from ReccipeListTableViewController

---
+++ RecipeApp/RecipeApp/View Controllers/RecipesForIngredientViewController.swift
@@ -0,0 +1,78 @@
+//
+//  RecipesForCategoryViewController.swift
+//  RecipeApp
+//
+//  Created by Nathan Wale on 15/5/2023.
+//
+
+import UIKit
+
+class RecipesForIngredientViewController: RecipeListTableViewController
+{
+    // MARK: - properties
+    let ingredient: Ingredient
+    var fetchTask: Task<Void, Never>? = nil
+    
+    
+    // MARK: - lifecycle
+    //
+    // Initialise with coder and ingredient
+    //
+    init?(
+        coder: NSCoder,
+        ingredient: Ingredient,
+        saveDelegate: SaveRecipeDelegate)
+    {
+        self.ingredient = ingredient
+        super.init(coder: coder)
+        self.saveRecipeDelegate = saveDelegate
+        saveRecipeDelegate.registerRecipeListController(self)
+    }
+    
+    
+    //
+    // Required initialiser, not used.
+    //
+    required init?(coder: NSCoder) {
+        fatalError("Not supported")
+    }
+    
+    
+    override func loadItems()
+    {
+        fetch()
+    }
+    
+    
+    // MARK: - remote
+    //
+    // Fetch recipes for category
+    //
+    func fetch()
+    {
+        // create search request
+        let request = RecipesForIngredientRequest(ingredient: ingredient)
+        
+        // cancel task if exists
+        fetchTask?.cancel()
+        
+        // create search task
+        fetchTask = Task {
+            do {
+                // fetch results and place in table
+                let items = try await request.fetchRecipes()
+                replaceItems(items: items)
+            } catch {
+                // on error empty table and print error
+                replaceItems(items: [])
+                print(error)
+            }
+            
+            // update datasource
+            updateDataSource()
+            
+            // finish task
+            fetchTask = nil
+        }
+    }
+}
