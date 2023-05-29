860ca402b5a71bfc6991b620a6d4a445119bada4
Nathan Wale <nathan.wale@icloud.com>
2023-05-29 16:49:20 +1000

    +NSUserActivity.navigation Extending NSUserActivity to store current AppNavigation

---
+++ RecipeApp/RecipeApp/Extensions/NSUserActivity.swift
@@ -0,0 +1,46 @@
+//
+//  NSUserActivity.swift
+//  RecipeApp
+//
+//  Created by Nathan Wale on 25/5/2023.
+//
+
+import Foundation
+
+/**
+Extend `NSUserActivity` to save navigation key
+*/
+extension NSUserActivity
+{
+    // activity type
+    static var activityType: String { "com.nathanwale.recipeapp.restoration"}
+    
+    // key to use for dictionary restoration
+    var navigationKey: String { "appNavigation" }
+    
+    // navigation state of the app
+    var navigation: AppNavigation
+    {
+        // decode from userInfo
+        get {
+            guard let jsonData = userInfo?[self.navigationKey] as? Data,
+                  let nav = try? JSONDecoder().decode(AppNavigation.self, from: jsonData)
+            else
+            {
+                // default state if we can't decode
+                return AppNavigation.defaultState()
+            }
+            
+            return nav
+        }
+        
+        // encode into userInfo
+        set {
+            if let jsonData = try? JSONEncoder().encode(newValue) {
+                userInfo?[navigationKey] = jsonData
+            } else {
+                userInfo?[navigationKey] = AppNavigation.defaultState()
+            }
+        }
+    }
+}
