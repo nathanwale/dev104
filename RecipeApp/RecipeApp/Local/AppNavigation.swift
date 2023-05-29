06625445d0ed0614afc171419cd90cca5979be6b
Nathan Wale <nathan.wale@icloud.com>
2023-05-29 16:44:03 +1000

    AppNavigation: a representation of the App's navigation state
    AppNavigation.Identifiers...:  storyboard identifiers for restoring view controllers
    
    AppNavigation.tabBarIndex: tab bar index for this navigation

---
+++ RecipeApp/RecipeApp/Local/AppNavigation.swift
@@ -0,0 +1,102 @@
+//
+//  AppNavigation.swift
+//  RecipeApp
+//
+//  Created by Nathan Wale on 24/5/2023.
+//
+
+import Foundation
+
+
+//
+// Representation of the app's navigation
+//
+enum AppNavigation: Codable
+{
+    case savedRecipes(SavedRecipes)
+    case search(Search)
+    case categories(Categories)
+    case ingredients(Ingredients)
+    
+    // Showing saved recipes
+    enum SavedRecipes: Codable
+    {
+        // Showing all recipes
+        case all
+        // Showing recipe with ID
+        case recipe(RecipeIdentifier)
+    }
+    
+    enum Search: Codable
+    {
+        // No search terms entered
+        case none
+        // Showing recipes for search term
+        case term(String)
+        // Showing recipe with search term and ID
+        case recipe(String, RecipeIdentifier)
+    }
+    
+    enum Categories: Codable
+    {
+        // Showing all categories
+        case all
+        // Showing recipes for category
+        case category(RecipeCategory)
+        // showing recipe with Category and recipe ID
+        case recipe(RecipeCategory, RecipeIdentifier)
+    }
+    
+    enum Ingredients: Codable
+    {
+        // Showing all ingredients
+        case all
+        // Showing recipes for ingredient
+        case ingredient(Ingredient)
+        // Showing recipe with ingredient and ID
+        case recipe(Ingredient, RecipeIdentifier)
+    }
+    
+    static func defaultState() -> Self
+    {
+        .savedRecipes(.all)
+    }
+    
+    
+    // MARK: - identifiers
+    //
+    // Storyboard IDs for View Controllers
+    //
+    struct Identifiers
+    {
+        static let savedRecipes = "SavedRecipes"
+        static let searchRecipes = "SearchRecipes"
+        static let categories = "Categories"
+        static let ingredients = "Ingredients"
+        static let recipesForCategory = "RecipesForCategory"
+        static let recipesForIngredient = "RecipesForIngredient"
+        static let recipeDetail = "RecipeDetail"
+    }
+}
+
+
+//
+// Tabbar index
+//
+extension AppNavigation
+{
+    var tabBarIndex: Int {
+        get {
+            switch self {
+                case .savedRecipes:
+                    return 0
+                case .search:
+                    return 1
+                case .categories:
+                    return 2
+                case .ingredients:
+                    return 3
+            }
+        }
+    }
+}
